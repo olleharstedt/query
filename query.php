6c924464219402620d3320e0a37c034e319ae8f8
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2023-10-11 00:09:24 +0200

    WIP

--- query.php
+++ query.php
@@ -288,12 +288,17 @@ function parseJson(array $json)
         $t->show();
         echo PHP_EOL;
         echo "(You're viewing result {$j})\n";
-        echo "(Click q to quit)\n";
+        echo "(Click q to quit, space to continue)\n";
+        back:
         readline_callback_handler_install("", function () { echo "here\n"; });
         readline_callback_read_char();
         $c = readline_info('line_buffer');
         if ($c === "q") {
             exit;
+        } elseif ($c === " ") {
+            continue;
+        } else {
+            goto back;
         }
     }
 }
