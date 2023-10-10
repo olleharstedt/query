c3b636812ff26544a8039a8808bdb02688bf93cc
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2023-10-10 22:23:04 +0200

    WIP

--- query.php
+++ query.php
@@ -8,11 +8,14 @@ require __DIR__.'/vendor/autoload.php';
 //ini_set('user_agent','Mozilla/4.0 (compatible; MSIE 6.0)');
 ini_set('user_agent','Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
 
+$config = include(__DIR__ . '/config.php');
+
 class Base
 {
     protected $href;
     public function __construct($href)
     {
+        echo "Making " . static::class . "\n";
         $this->href = $href;
     }
 
@@ -39,6 +42,9 @@ class Base
     protected function getLink()
     {
         $parts = explode("=", $this->href);
+        if (count($parts) === 1) {
+            return $this->href;
+        }
         $parts = explode("&", $parts[1]);
         return urldecode($parts[0]);
     }
@@ -57,7 +63,7 @@ class Stackoverflow extends Base
             echo "POSTCELL\n";
             echo $crawler->filter("div.postcell")->text() . PHP_EOL;
             echo "ANSWERCELLS\n";
-            echo $crawler->filter("div.answercell")->value() . PHP_EOL;
+            echo $crawler->filter("div.answercell")->text(true) . PHP_EOL;
 
             //$dom = new DOMDocument();
             //@$dom->loadHTML($content);
@@ -136,15 +142,21 @@ class Factory
     public function make($href)
     {
         $parts = explode("=", $href);
-        $parts = explode("/", $parts[1]);
+        if (count($parts) === 1) {
+            $parts = explode("/", $href);
+            $key = $parts[2] ?? '';
+        } else {
+            $parts = explode("/", $parts[1]);
+            $key = $parts[2] ?? '';
+        }
         $map = [
             "www.quora.com" => Query::class,
             "www.youtube.com" => YouTube::class,
             "support.google.com" => Silent::class,
             "stackoverflow.com" => Stackoverflow::class,
         ];
-        if (isset($map[$parts[2] ?? ''])) {
-            return new $map[$parts[2]]($href);
+        if (isset($map[$key])) {
+            return new $map[$key]($href);
         } else {
             return new Unknown($href);
         }
@@ -158,57 +170,92 @@ for ($i = 1; $i < count($argv); $i++) {
 //echo $query . PHP_EOL;
 $query = urlencode($query);
 
+//https://www.googleapis.com/customsearch/v1?[parameters]
 $useragent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13';
 $ch = curl_init("");
-curl_setopt($ch, CURLOPT_URL, "https://google.com/search?q=$query");
+$params = [
+    'key' => $config['key'],
+    'cx'  => $config['cx'],
+    'q'   => $query
+];
+
+$url = "https://www.googleapis.com/customsearch/v1?" . http_build_query($params);
+curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_USERAGENT, $useragent); // set user agent
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 $content = curl_exec($ch);
-var_dump(curl_error($ch));
+//var_dump(curl_error($ch));
 curl_close($ch);
-echo $content;
+if ($content) {
+    $result = json_decode($content, true);
+    parseJson($result);
+}
 
 //$content = file_get_contents("https://google.com/search?q=$query");
-$dom = new DOMDocument();
-@$dom->loadHTML($content);
-$fac = new Factory();
-foreach ($dom->getElementsByTagName("a") as $a) {
-    if ($a->textContent) {
-        $href = $a->getAttribute("href");
-        if (strpos($href, "url") !== 1) {
-            continue;
+function parseHtml($content)
+{
+    $dom = new DOMDocument();
+    @$dom->loadHTML($content);
+    $fac = new Factory();
+    foreach ($dom->getElementsByTagName("a") as $a) {
+        if ($a->textContent) {
+            $href = $a->getAttribute("href");
+            if (strpos($href, "url") !== 1) {
+                continue;
+            }
+            //echo $href . PHP_EOL;
+            $t = $fac->make($href);
+            $t->show();
+            //echo $a->textContent . PHP_EOL;
+            $parent = $a->parentNode;
+            //$sibling = $parent->nextSibling;
+            //echo PHP_EOL;
+            //echo $parent->textContent . PHP_EOL;
+            $sibling = $a->nextSibling;
+            if ($sibling) {
+                //echo $sibling->textContent . PHP_EOL;
+            }
+            //echo $href . PHP_EOL;
+            echo PHP_EOL;
+            //if (strpos($href
+
+
+            //$answer = fgets(STDIN);
+
+            //$handle = fopen ("php://stdin","r");
+            //$line = fgets($handle);
+            //fclose($handle);
+
+            // Get the standard input stream.
+            //$stdin = fopen("php://stdin", "r");
+            // Read a single character from the stream.
+            //$keystroke = fread($stdin, 1);
+            // Close the stream.
+            //fclose($stdin);
+
+            readline_callback_handler_install("", function () { echo "here\n"; });
+            readline_callback_read_char();
+            $c = readline_info('line_buffer');
+            if ($c === "q") {
+                exit;
+            }
         }
+    }
+}
+
+// $json coming from google api
+function parseJson(array $json)
+{
+    echo "Parse json\n";
+    $fac = new Factory();
+    foreach ($json['items'] as $item) {
+        echo "Process item\n";
+        $href = $item['link'];
         //echo $href . PHP_EOL;
         $t = $fac->make($href);
         $t->show();
-        //echo $a->textContent . PHP_EOL;
-        $parent = $a->parentNode;
-        //$sibling = $parent->nextSibling;
-        //echo PHP_EOL;
-        //echo $parent->textContent . PHP_EOL;
-        $sibling = $a->nextSibling;
-        if ($sibling) {
-            //echo $sibling->textContent . PHP_EOL;
-        }
-        //echo $href . PHP_EOL;
         echo PHP_EOL;
-        //if (strpos($href
-
-
-        //$answer = fgets(STDIN);
-
-        //$handle = fopen ("php://stdin","r");
-        //$line = fgets($handle);
-        //fclose($handle);
-
-        // Get the standard input stream.
-        //$stdin = fopen("php://stdin", "r");
-        // Read a single character from the stream.
-        //$keystroke = fread($stdin, 1);
-        // Close the stream.
-        //fclose($stdin);
-
         readline_callback_handler_install("", function () { echo "here\n"; });
         readline_callback_read_char();
         $c = readline_info('line_buffer');
