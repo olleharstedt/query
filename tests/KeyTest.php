0a2b7fded4ddcae7e84cd8c9f03d985d85a7268d
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2023-10-15 22:55:20 +0200

    WIP

--- tests/KeyTest.php
+++ tests/KeyTest.php
@@ -2,16 +2,24 @@
 
 use PHPUnit\Framework\TestCase;
 use Query\Factory;
+use Query\IO;
 
 require __DIR__.'/../vendor/autoload.php';
 require __DIR__.'/../src/functions.query.php';
 
 class KeyTest extends TestCase
 {
+    /**
+     * @covers Query\Factory::make
+     */
     public function testKey()
     {
-        $f = new Factory();
-        $res = $f->make("/url?q=https://stackoverflow.com/rotmos&sa=U&ved=2ahUKEwi04--39PCBAxWhcfEDHdukDCoQFnoECAEQBA&usg=AOvVaw1-CR3wteujT38AIxfp5IFq");
+        $f = new Factory(new IO());
+        $url = "/url?q=https://stackoverflow.com/rotmos&sa=U&ved=2ahUKEwi04--39PCBAxWhcfEDHdukDCoQFnoECAEQBA&usg=AOvVaw1-CR3wteujT38AIxfp5IFq";
+        $res = $f
+            ->make()
+            ->with($url)
+            ->run();
         $this->assertEquals(get_class($res), "Query\Stackoverflow");
     }
 }
