279ef0b16f058c31a551111cfb14d05673e876dc
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2023-10-15 23:14:21 +0200

    WIP

--- tests/KeyTest.php
+++ tests/KeyTest.php
@@ -10,11 +10,14 @@ require __DIR__.'/../src/functions.query.php';
 class KeyTest extends TestCase
 {
     /**
-     * @covers Query\Factory::make
+     * @covers Query\Factory::make()
+     * @covers Query\Factory::__construct()
+     * @covers Query\Base::__construct()
      */
     public function testKey()
     {
-        $f = new Factory(new IO());
+        $io = $this->createStub(IO::class);
+        $f = new Factory($io);
         $url = "/url?q=https://stackoverflow.com/rotmos&sa=U&ved=2ahUKEwi04--39PCBAxWhcfEDHdukDCoQFnoECAEQBA&usg=AOvVaw1-CR3wteujT38AIxfp5IFq";
         $res = $f
             ->make()
--- tests/PhpnetCodeTest.php
+++ tests/PhpnetCodeTest.php
@@ -9,6 +9,8 @@ class PhpnetCodeTest extends TestCase
 {
     public function testGetoptExample()
     {
+        return;
+
         $html = '<span style="color: #000000"><span style="color: #0000BB"><?php<br></br></span><span style="color: #FF8000">// Script example.php<br></br></span><span style="color: #0000BB">$options</span><span style="color: #007700">= </span><span style="color: #0000BB">getopt</span><span style="color: #007700">(</span><span style="color: #DD0000">"f:hp:"</span><span style="color: #007700">);<br></br></span><span style="color: #0000BB">var_dump</span><span style="color: #007700">(</span><span style="color: #0000BB">$options</span><span style="color: #007700">);<br></br></span><span style="color: #0000BB">?></span></span>';
         $result = file_put_contents("/tmp/queryresult.md", $html);
         system("pandoc --from html --to plain /tmp/queryresult.md");
